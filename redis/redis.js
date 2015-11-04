64a7d5d9e5f6723bbeb7ddc9e5e8b0fe92731e28
Eddie Huang <inoc603@gmail.com>
2015-11-04 22:37:35 +0800

    limit listeners number

--- redis/redis.js
+++ redis/redis.js
@@ -1,91 +1,88 @@
 module.exports = function(RED) {
-    "use strict";
-    var util = require("util");
-    var redis = require("redis");
+  'use strict'
+  var redis = require('redis')
 
-    var hashFieldRE = /^([^=]+)=(.*)$/;
-    // var validCmd = [
-    //   'sadd', 'spop', 'smembers', 'sismember'
-    // ]
+  const CONNECTED = {fill: 'green', shape: 'dot', text: 'connected'}
+      , DISCONNECTED = {fill: 'red', shape: 'ring', text: 'disconnected'}
 
-    var redisConnectionPool = function() {
-        var connections = {};
-        var obj = {
-            get: function(host,port) {
-                var id = host+":"+port;
-                if (!connections[id]) {
-                    connections[id] = redis.createClient(port,host);
-                    connections[id].on("error",function(err) {
-                            util.log("[redis] "+err);
-                    });
-                    connections[id].on("connect",function() {
-                            util.log("[redis] connected to "+host+":"+port);
-                    });
-                    connections[id]._id = id;
-                    connections[id]._nodeCount = 0;
-                }
-                connections[id]._nodeCount += 1;
-                return connections[id];
-            },
-            close: function(connection) {
-                connection._nodeCount -= 1;
-                if (connection._nodeCount === 0) {
-                    if (connection) {
-                        clearTimeout(connection.retry_timer);
-                        connection.end();
-                    }
-                    delete connections[connection._id];
-                }
-            }
-        };
-        return obj;
-    }();
+  var redisConnectionPool = function() {
+    var connections = {}
+    var obj = {
+      get: function(host, port) {
+        var id = host + ':' + port
+        if (!connections[id]) {
+          var conn = redis.createClient(port, host)
+          conn.nodes = []
+          conn._id = id
+          conn._nodeCount = 0
 
+          conn.on('error', function(err) {
+            console.log('[redis] '+ err)
+          })
 
-    function RedisInNode(n) {
-        RED.nodes.createNode(this,n);
-        this.port = n.port||"6379";
-        this.hostname = n.hostname||"127.0.0.1";
-        this.key = n.key;
-        this.structtype = n.structtype;
-
-        this.client = redisConnectionPool.get(this.hostname,this.port);
-
-        if (this.client.connected) {
-            this.status({fill:"green",shape:"dot",text:"connected"});
-        } else {
-            this.status({fill:"red",shape:"ring",text:"disconnected"},true);
-        }
-
-        var node = this;
-        this.client.on("end", function() {
-            node.status({fill:"red",shape:"ring",text:"disconnected"});
-        });
-        this.client.on("connect", function() {
-            node.status({fill:"green",shape:"dot",text:"connected"});
-        });
-
-        this.on("input", function(msg) {
-            var k = this.key || msg.topic;
-            var cmd = msg.payload[0]
-            var args = msg.payload
-            args.shift()
-
-            this.client.send_command(cmd, args, function (err, reply) {
-              if (err) {
-                msg.payload = err
-              }
-              else {
-                msg.payload = reply
-              }
-              node.send(msg)
+          conn.on('end', function () {
+            conn.nodes.map(function (node) {
+              node.status(DISCONNECTED)
             })
+          })
+
+          conn.on('connect', function () {
+            console.log('[redis] connected to ' + host + ':' + port)
+            conn.nodes.map(function (node) {
+              node.status(CONNECTED)
+            })
+          })
+
+          connections[id] = conn
+        }
+        connections[id]._nodeCount += 1
+        return connections[id]
+      },
+
+      close: function(connection, nodeId) {
+        connection._nodeCount -= 1
+        connection.nodes = connection.nodes.filter(function (node) {
+          return node.id !== nodeId
         })
-
-
-        this.on("close", function() {
-            redisConnectionPool.close(node.client);
-        });
+        if (connection._nodeCount === 0) {
+          if (connection) {
+            clearTimeout(connection.retry_timer)
+            connection.end()
+          }
+          delete connections[connection._id]
+        }
+      }
     }
-    RED.nodes.registerType("redis cmd",RedisInNode);
+    return obj
+  }()
+
+  function RedisCmdNode(n) {
+    RED.nodes.createNode(this, n)
+    var node = this
+    node.port = n.port || '6379'
+    node.hostname = n.hostname || '127.0.0.1'
+
+    node.client = redisConnectionPool.get(node.hostname, node.port)
+    node.client.nodes.push(node)
+
+    node.status(node.client.connected ? CONNECTED : DISCONNECTED)
+
+    node.on('input', function(msg) {
+      var cmd = msg.payload[0]
+        , args = msg.payload
+      args.shift()
+
+      node.client.send_command(cmd, args, function (err, reply) {
+        msg.error = err
+        msg.payload = reply
+        node.send(msg)
+      })
+    })
+
+    node.on('close', function() {
+      redisConnectionPool.close(node.client, node.id)
+    })
+  }
+
+  RED.nodes.registerType('redis cmd', RedisCmdNode)
 }
